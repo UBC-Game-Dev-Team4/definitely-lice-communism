0d94049bbea79f32847172daaa62235bc258bb12
Z0rb14n <babybluewoof@gmail.com>
2021-11-21 18:13:45 -0800

    Implement Music Fade Out for Rooms with BGM

--- Assets/Scripts/AreaScript.cs
+++ Assets/Scripts/AreaScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using DefaultNamespace;
+using Sound;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Util
 {
@@ -11,6 +13,18 @@ namespace Util
     {
         [Tooltip("Player's camera state on enter")]
         public CameraState cameraStateOnEnter = CameraState.CreateFreeXYCameraState(new Vector3(0, 0, -10));
+
+        [Tooltip("Background music to play upon enter?")]
+        public AudioSource backgroundMusic;
+
+        [Tooltip("Background Music Target Audio Mixer")]
+        public AudioMixerGroup musicTargetAudioMixer;
+
+        [Tooltip("Background music fade audio mixer")]
+        public AudioMixerGroup musicFadeAudioMixer;
+
+        [Tooltip("Background Music audio mixer")]
+        public AudioMixer audioMixer;
         
         private DoorScript[] _doors;
         private AreaScript[] _adjacentAreas;
@@ -19,6 +33,44 @@ namespace Util
         {
             _doors = transform.GetComponentsInChildren<DoorScript>();
             _adjacentAreas = _doors.Select(door => door.areaToTeleportTo).Where(door => door != null).ToArray();
+        }
+
+        /// <summary>
+        /// Begins to fade out the background music, with a given fade duration.
+        /// </summary>
+        /// <param name="fadeoutDuration">Time it takes to fade out the music</param>
+        public void FadeBackgroundMusic(float fadeoutDuration = 1)
+        {
+            if (backgroundMusic == null || audioMixer == null) return;
+            backgroundMusic.outputAudioMixerGroup = musicFadeAudioMixer;
+            StartCoroutine(SoundManager.StartMusicFadeOut(backgroundMusic, audioMixer, fadeoutDuration, true));
+        }
+
+        
+        /// <summary>
+        /// Determines whether this area has its own background music.
+        /// </summary>
+        /// <returns>Boolean of whether background music is defined</returns>
+        public bool HasBackgroundMusic() => backgroundMusic != null;
+
+        /// <summary>
+        /// Starts playing background music, if it exists
+        /// </summary>
+        public void PlayBackgroundMusic()
+        {
+            if (backgroundMusic == null) return;
+            if (musicTargetAudioMixer != null)
+                backgroundMusic.outputAudioMixerGroup = musicTargetAudioMixer;
+            backgroundMusic.Play();
+        }
+
+        /// <summary>
+        /// On editor value change, log an error if the music fade audio mixer is not defined
+        /// </summary>
+        public void OnValidate()
+        {
+            if (musicFadeAudioMixer == null)
+                Debug.LogError("Music Fade Audio Mixer should not be null.");
         }
     }
 }
