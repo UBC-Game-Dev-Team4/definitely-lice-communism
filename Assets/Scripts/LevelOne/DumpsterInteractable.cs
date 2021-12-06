50fa1263369c6ca287e136a95fc234dd1f422cb9
Iris Xu <irisxu02@gmail.com>
2021-12-05 17:22:18 -0800

    Create DumpsterInteractable.cs

---
+++ Assets/Scripts/LevelOne/DumpsterInteractable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DefaultNamespace;
+using ItemInventory;
+
+namespace LevelOne
+{
+    /// <summary>
+    ///     A dumpster that is interactable. Gives player an item upon interaction.
+    /// </summary>
+    public class DumpsterInteractable
+    {
+        public bool isInteractable;
+        public Item item;
+        private bool _pickedUp;
+        public override void Interact(object src, params object[] args)
+        {
+            if (_pickedUp) return;
+            if (!isInteractable) return;
+            base.Interact(src, args);
+            Inventory.Instance.Add(item);
+            Destroy(gameObject);
+            _pickedUp = true;
+        }
+    }
+}
