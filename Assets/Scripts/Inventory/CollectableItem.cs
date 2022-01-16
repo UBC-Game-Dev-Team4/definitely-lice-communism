85479e5ebddab94851b8c93d4c194ca8c387ae97
Z0rb14n <babybluewoof@gmail.com>
2022-01-16 11:11:10 -0800

    Dead chef uses new sprite; delete test object

--- Assets/Scripts/Inventory/CollectableItem.cs
+++ Assets/Scripts/Inventory/CollectableItem.cs
@@ -5,6 +5,7 @@ namespace ItemInventory
     public class CollectableItem : Interactable
     {
         public Item item;
+        public bool destroyOnPickup = true;
         private bool _pickedUp;
 
         public override void Interact(object src, params object[] args)
@@ -12,7 +13,8 @@ namespace ItemInventory
             if (_pickedUp) return;
             base.Interact(src, args);
             Inventory.Instance.Add(item);
-            Destroy(gameObject);
+            if (destroyOnPickup)
+                Destroy(gameObject);
             _pickedUp = true;
         }
     }
