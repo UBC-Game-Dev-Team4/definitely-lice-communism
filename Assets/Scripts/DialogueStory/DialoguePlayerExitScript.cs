dfad6d24127ea56c44b8b86302a5a929513f84d6
Z0rb14n <babybluewoof@gmail.com>
2022-01-16 16:09:40 -0800

    Adjust cat animation speed; dialogue exits upon leaving area

---
+++ Assets/Scripts/DialogueStory/DialoguePlayerExitScript.cs
@@ -0,0 +1,19 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DialogueStory
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class DialoguePlayerExitScript : MonoBehaviour
+    {
+        [Tooltip("Unity event called on exit")]
+        public UnityEvent eventOnExit = new UnityEvent();
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            DialogueManager.Instance.Close();
+            PlayerScript.Instance.interactionEnabled = true;
+            eventOnExit.Invoke();
+        }
+    }
+}
