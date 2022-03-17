c121049061c7b5cbf3703e920ff7f939b38c2e63
irisxu02 <45346203+irisxu02@users.noreply.github.com>
2022-03-16 20:49:54 -0700

    Iris scene1 plate interactable (#69)
    * PlateInteractable.cs added to LevelOne
    
    * modified summary comments and added item field

---
+++ Assets/Scripts/LevelOne/PlateInteractable.cs
@@ -0,0 +1,36 @@
+﻿using DefaultNamespace;
+using ItemInventory;
+
+namespace LevelOne
+{
+    /// <summary>
+    ///     Plate is a collectable item that can break.
+    /// </summary>
+    public class PlateInteractable : CollectableItem
+    {
+        public bool isInteractable;
+        public Item item;
+        private bool _pickedUp;
+        private bool _broken;
+
+        public override void Interact(object src, params object[] args)
+        {
+            if (_pickedUp) return;
+            if (!isInteractable) return;
+            if (_broken) return;
+            base.Interact(src, args);
+            _broken = false;
+        }
+        
+        /// <summary>
+        /// breaks the plate, item is dropped and is no longer interactable
+        /// </summary>
+        public void Break()
+        {
+            _broken = true;
+            _pickedUp = false;
+            isInteractable = false;
+            Inventory.Instance.Remove(item);
+        }
+    }
+}
