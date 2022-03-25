ef8e6a4cb71d85a57dd3d8c42d7a9d69247d9f35
Z0rb14n <babybluewoof@gmail.com>
2022-03-24 19:04:16 -0700

    Team logo; move spilled oil to level 1; deep fryer script preliminary impl

---
+++ Assets/Scripts/LevelOne/DeepFryerScript.cs
@@ -0,0 +1,40 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace LevelOne
+{
+    /// <summary>
+    /// Script attached to the deep fryer to handle killing logic
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class DeepFryerScript : Interactable
+    {
+        private ChefScript _chef = null;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            ChefScript chef = other.gameObject.GetComponent<ChefScript>();
+            if (chef != null)
+            {
+                _chef = chef;
+            }
+        }
+
+        public override void Interact(object src, params object[] args)
+        {
+            base.Interact(src, args);
+            if (_chef != null)
+            {
+                _chef.Kill(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            ChefScript chef = other.gameObject.GetComponent<ChefScript>();
+            if (chef != null)
+            {
+                _chef = null;
+            }
+        }
+    }
+}
