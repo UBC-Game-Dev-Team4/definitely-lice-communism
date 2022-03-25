d7904959a9fd517a53f6e945bd3ed04c56234b03
Z0rb14n <babybluewoof@gmail.com>
2022-03-24 19:49:02 -0700

    Implement plate break/killing chef

--- Assets/Scripts/LevelOne/PlateInteractable.cs
+++ Assets/Scripts/LevelOne/PlateInteractable.cs
@@ -7,28 +7,11 @@ namespace LevelOne
     /// </summary>
     public class PlateInteractable : CollectableItem
     {
-        public bool isInteractable;
-        private bool _broken;
-
         public override void Interact(object src, params object[] args)
         {
             if (_pickedUp) return;
-            if (!isInteractable) return;
-            if (_broken) return;
             base.Interact(src, args);
             LevelOneInfoStorer.CastedSingleton.CastedInfo.wasPlatePickedUp = true;
-            _broken = false;
-        }
-        
-        /// <summary>
-        /// breaks the plate, item is dropped and is no longer interactable
-        /// </summary>
-        public void Break()
-        {
-            _broken = true;
-            _pickedUp = false;
-            isInteractable = false;
-            Inventory.Instance.Remove(item);
         }
     }
 }
